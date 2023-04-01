0374b936db004c17c5b8cc0f586739b4f4418d63
rafael <rafaelrochasouza@outlook.com.br>
2023-04-01 18:11:09 -0300

    add: functional units creation

--- scoreboard/functionalUnit.cpp
+++ scoreboard/functionalUnit.cpp
@@ -3,4 +3,13 @@
 
 FunctionalUnit::FunctionalUnit (FunctionalUnitTypes fuType) {
   type = fuType;
+  isBusy = false;
+  operation = "";
+  destinationRegister = "";
+  sourceOperand1 = "";
+  sourceOperand2 = "";
+  producingUnit1 = "";
+  producingUnit2 = "";
+  isFirstOperandReady = false;
+  isSecondOperandReady = false;
 }
--- scoreboard/main.cpp
+++ scoreboard/main.cpp
@@ -1,10 +1,51 @@
 #include <iostream>
 #include <string>
+#include <vector>
 
-#include "./consts.cpp"
+#include "consts.cpp"
+#include "scoreboard.hpp"
+#include "functionalUnitTypesEnum.cpp"
 
-int main() {
-  std::cout << INITIAL_STRING << std::endl;
+struct DefaultFunctionalUnit {
+  int amount;
+  FunctionalUnitTypes type;
+};
 
-  
+std::vector<DefaultFunctionalUnit> instantiateDefaultUnitsObject () {
+  std::cout << "[+] Instantiating default functional units..." << std::endl;
+  std::vector<DefaultFunctionalUnit> defaultUnits;
+
+  defaultUnits.push_back({
+    amount : 3,
+    type : FunctionalUnitTypes::INT
+  });
+  defaultUnits.push_back({
+    amount : 2,
+    type : FunctionalUnitTypes::F_ADD
+  });
+  defaultUnits.push_back({
+    amount : 2,
+    type : FunctionalUnitTypes::F_MULTIPLY
+  });
+  defaultUnits.push_back({
+    amount : 2,
+    type : FunctionalUnitTypes::F_DIVISION
+  });
+
+  return defaultUnits;
+}
+
+int main () {
+  std::cout << STARS << std::endl << INITIAL_STRING <<  STARS << std::endl << std::endl;
+
+  Scoreboard *scoreboard = new Scoreboard();
+
+  std::cout << "[!] No functional units input detected, using default ones..." << std::endl;
+  const std::vector<DefaultFunctionalUnit> defaultUnits = instantiateDefaultUnitsObject();
+
+  for (size_t i = 0; i < defaultUnits.size(); i++) {
+    scoreboard->createFunctionalUnits(defaultUnits[i].amount, defaultUnits[i].type);
+  }
+
+  scoreboard->printFunctionalUnits();
 }
